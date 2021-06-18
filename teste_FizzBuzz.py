5b378920fe21d4b2a9d944cc13cce375690d9822
jbaronio <josebaronio@gmail.com>
2021-06-18 16:00:13 -0300

    FizzBuzz retorna Buzz para 53

--- teste_FizzBuzz.py
+++ teste_FizzBuzz.py
@@ -6,6 +6,8 @@ def test_should_return_1_for_1():
 def fizz_buzz(i):
     if i == 3:
         return "Fizz"
+    if i == 5:
+        return "Buzz"
     return i
 
 def test_should_return_2_for_2():
@@ -13,3 +15,6 @@ def test_should_return_2_for_2():
 
 def test_should_return_fizz_for_3():
     assert "Fizz" == fizz_buzz(3)
+
+def test_should_return_buzz_for_5():
+    assert "Buzz" == fizz_buzz(5)
