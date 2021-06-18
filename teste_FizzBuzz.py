431d5c54ef29e251ad149cde8bdac4f3f8ff42a2
jbaronio <josebaronio@gmail.com>
2021-06-18 16:15:37 -0300

    FizzBuzz retorna Fizz para multiplos de 3

--- teste_FizzBuzz.py
+++ teste_FizzBuzz.py
@@ -1,20 +1,19 @@
 import pytest
 
-def test_should_return_1_for_1():
-    assert 1 == fizz_buzz(1)
-
 def fizz_buzz(i):
-    if i == 3:
+    if i % 3 == 0:
         return "Fizz"
     if i == 5:
         return "Buzz"
     return i
 
-def test_should_return_2_for_2():
+def test_should_return_number_for_number():
+    assert 1 == fizz_buzz(1)
     assert 2 == fizz_buzz(2)
 
-def test_should_return_fizz_for_3():
+def test_should_return_fizz_for_multiple_of_3():
     assert "Fizz" == fizz_buzz(3)
+    assert "Fizz" == fizz_buzz(6)
 
 def test_should_return_buzz_for_5():
     assert "Buzz" == fizz_buzz(5)
